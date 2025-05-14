66ddd099badd57c450412144f7487e1e49f2a679
peter4tech <peter4tech@gmail.com>
2025-05-14 16:15:44 +0100

    ProjectFiles

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  
+<div>
+  <h1 className="bg-red-900"> This is Working now </h1>
+</div>
+  )
+}
+
+export default App
