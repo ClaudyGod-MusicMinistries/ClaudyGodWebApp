df958b868bc034bad81f36faf2c313054fb91e5f
peter4tech <peter4tech@gmail.com>
2025-05-15 08:34:13 +0100

    TailwindC0nfigurationWorking

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 
 import './App.css'
 
 function App() {
- 
 
   return (
-  
-<div>
-  <h1 className="bg-red-900"> Testing the github </h1>
-</div>
+    <h1 className='text-center'> Welcome </h1>
   )
 }
 
