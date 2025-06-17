95048a016da13936ef0ca381a80a78b53a6201ff
Ogba Peter Chima <peter@skilledup.life>
2025-06-17 03:15:38 +0100

    ChangesTobackend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("checking again");
+  res.send("testing the backend for changes");
 });
 
 app.listen(port, () => {
