9d2b1a2734eb605ac22ce39cc775effbb26a82c9
Ogba Peter Chima <peter@skilledup.life>
2025-06-18 10:58:00 +0100

    testing production

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,14 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
+// // Handle preflight requests
+// app.options('*', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.sendStatus(204);
+// });
 // === Middleware ===
 app.use(helmet());
 app.use(compression());
