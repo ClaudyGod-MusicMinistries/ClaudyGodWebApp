27673b94980d7a97317939ce18dad325020edf71
Ogba Peter Chima <peter@skilledup.life>
2025-06-21 05:15:10 +0100

    logoFix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/',
+  publicDir: 'public',
+
   plugins: [
     react(),
     tailwindcss(),
