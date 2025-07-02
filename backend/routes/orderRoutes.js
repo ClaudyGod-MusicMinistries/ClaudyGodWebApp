4bcecbb1404ddd7d867478633ee1b50828ce5de1
Ogba Peter Chima <peter@skilledup.life>
2025-07-02 18:44:58 +0100

    Frontend-working

--- backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/orderRoutes.js
-const express    = require("express");
-const router     = express.Router();
-const orderCtrl  = require("../controllers/OrderController");
-
-router.post("/", orderCtrl.createOrder);
-router.post("/:orderId/confirm-payment", orderCtrl.confirmPayment);
-
-module.exports = router;
