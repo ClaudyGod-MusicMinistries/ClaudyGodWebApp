a33e9aad71f394c274a866ac1c6e24767048b902
Ogba Peter Chima <peter@skilledup.life>
2025-08-28 09:23:52 +0100

    quickFix

--- frontend/src/components/Homepage/ShopPreview.tsx
+++ frontend/src/components/Homepage/ShopPreview.tsx
@@ -239,7 +239,7 @@ export const ShopPreview: React.FC = () => {
             fontSize="16px"
             className="max-w-2xl mx-auto"
           >
-            Premium merchandise crafted for your everyday Hse
+            Premium merchandise crafted for your everyday use
           </LightText>
         </motion.div>
 
